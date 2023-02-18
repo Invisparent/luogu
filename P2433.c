943370a3c90a6f6b357efe500195fae713e6e703
Invisparent <invisparent@outlook.com>
2023-02-18 13:52:39 +0800

    Create P2433.c

---
+++ P2433.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main() {
+    int tt;
+    scanf("%d", &tt);
+    switch (tt) {
+        case 1:
+            printf("I love Luogu!");
+            break;
+        case 2:
+            printf("6 4");
+            break;
+        case 3:
+            printf("3\n12\n2");
+            break;
+        case 4:
+            printf("166.667");
+            break;
+        case 5:
+            printf("15");
+            break;
+        case 6:
+            printf("10.8167");
+            break;
+        case 7:
+            printf("110\n90\n0");
+            break;
+        case 8:
+            printf("31.4159\n78.5398\n523.599");
+            break;
+        case 9:
+            printf("22");
+            break;
+        case 10:
+            putchar('9');
+            break;
+        case 11:
+            printf("33.3333");
+            break;
+        case 12:
+            printf("13\nR");
+            break;
+        case 13:
+            printf("16");
+            break;
+        case 14:
+            printf("50");
+            break;
+        default:
+            break;
+    }
+    return 0;
+}
