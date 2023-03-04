0d9b1bf4ee4e501689d45f3d6a45cad9139e4516
Invisparent <invisparent@outlook.com>
2023-03-04 12:12:16 +0800

    Create P1303.java

---
+++ P1303.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner ivsScan = new Scanner(System.in);
+        BigInteger a = ivsScan.nextBigInteger();
+        BigInteger b = ivsScan.nextBigInteger();
+
+        System.out.println(a.multiply(b));
+    }
+}
